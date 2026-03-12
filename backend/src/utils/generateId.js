fb1ddd102e235a6faf3ac7410c2c3085616a4a09
AnoudMD <AlanoudAlmouteri@gmail.com>
2026-03-12 06:58:30 +0300

    dd

--- backend/src/utils/generateId.js
+++
@@ -1,5 +0,0 @@
-function generateId(prefix) {
-  return `${prefix}-${Date.now()}-${Math.floor(Math.random() * 1000)}`;
-}
-
-module.exports = generateId;
--- backend/src/utils/qr.js
+++
@@ -1,5 +0,0 @@
-function makeQrValue(batchId) {
-  return `http://localhost:3000/api/scan/${batchId}`;
-}
-
-module.exports = { makeQrValue };
