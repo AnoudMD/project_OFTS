2516e67beb6f1e4575a8157325157b46558ba501
Almaha Alwuthaylah <almahaalwthelah@gmail.com>
2026-03-12 08:08:02 +0300

    storage

---
+++ frontend/src/utils/constants.js
@@ -0,0 +1,4 @@
+export const API_BASE_URL = 'http://192.168.1.10:5000/api';
+export const ROLES = ['Producer', 'Certifier', 'Distributor', 'Retailer'];
+export const EVENT_TYPES = ['Harvest', 'Processing', 'Quality Check',
+'Packaging', 'Shipment', 'Distribution'];
