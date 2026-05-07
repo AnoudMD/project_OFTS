881c40ed9496295d2d7ddce7012533259168d8c4
njoudNAT <njoudaltalal@gmail.com>
2026-05-07 18:24:18 +0300

    deletebackups

--- frontend_backup_before_ios_device/src/api/traceApi.js
+++
@@ -1,5 +0,0 @@
-import client from './client';
-export const getTraceabilityApi = async (batchId) => {
-const { data } = await client.get(`/trace/${batchId}`);
-return data;
-};
--- frontend_backup_before_ios_device/src/utils/constants.js
+++
@@ -1,6 +0,0 @@
-const rawApiBaseUrl =
-  process.env.EXPO_PUBLIC_API_URL || 'http://localhost:5001/api';
-
-export const API_BASE_URL = rawApiBaseUrl.replace(/\/+$/, '');
-export const ROLES = ['Producer', 'Certifier', 'Distributor', 'Retailer'];
-export const EVENT_TYPES = ['Harvest', 'Processing', 'Quality Check', 'Packaging', 'Shipment', 'Distribution'];
